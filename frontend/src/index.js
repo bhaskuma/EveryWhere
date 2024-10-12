9f9c7847100d665e9ed0ef26545623ca8d8abd00
bhaskuma <bhaskarkumarrajput100@gmail.com>
2024-10-12 16:18:27 +0530

    add redux storage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,22 +5,22 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './redux/store';
 import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-
-
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,13 +1,34 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice'
+import userReducer from './user/userSlice';
 import subscriptionReducer from './subscription/subscriptionSlice';
 import serviceproviderReducer from './serviceProvider/serviceProviderSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Default storage (localStorage for web)
+
+// Configuration for redux-persist
+const persistConfig = {
+    key: 'root', // Key to store the persisted data
+    storage,     // Use local storage
+};
+
+// Create a persisted reducer for each slice
+const persistedUserReducer = persistReducer(persistConfig, userReducer);
+const persistedSubscriptionReducer = persistReducer(persistConfig, subscriptionReducer);
+const persistedServiceProviderReducer = persistReducer(persistConfig, serviceproviderReducer);
+
 const store = configureStore({
     reducer: {
-        user: userReducer,
-        subscription: subscriptionReducer,
-        serviceprovider: serviceproviderReducer
+        user: persistedUserReducer,               // Persisting user slice
+        subscription: persistedSubscriptionReducer, // Persisting subscription slice
+        serviceprovider: persistedServiceProviderReducer, // Persisting serviceprovider slice
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,  // Disable serializability check for redux-persist
+        }),
 });
+
+// Persistor for the store
+export const persistor = persistStore(store);
 
 export default store;
